f38e30fb0b21fc6a487c100e3d41b12fc96e40af
erlafont <eric.lafontaine1@gmail.com>
2018-02-07 20:16:58 -0500

    refactor Problem3 to show solution

--- Problem3.py
+++ Problem3.py
@@ -2,12 +2,6 @@ import math
 
 __author__ = 'erlafont'
 
-if __name__ == '__main__':
-    print(__file__, " is main!")
-else:
-    print(__file__, " is loaded!")
-
-
 def is_prime(number):
     for index in range(2,math.ceil(math.sqrt(number))+1):
         if number % index == 0 and number != index:
@@ -21,3 +15,11 @@ def find_all_prime_factors(number):
         if is_prime(index) and number % index == 0 and number != index:
             list_of_primes.append(index)
     return list_of_primes
+
+
+if __name__ == '__main__':
+    print(__file__, " is main!")
+    print(find_all_prime_factors(600851475143))
+else:
+    print(__file__, " is loaded!")
+
